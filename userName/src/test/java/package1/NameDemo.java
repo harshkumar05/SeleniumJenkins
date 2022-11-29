aa75788daded9c00917e51f6b78e6fdbb441e444
Harsh <57654076+harshkumar05@users.noreply.github.com>
2022-11-30 00:43:22 +0530

    Add files via upload

---
+++ userName/src/test/java/package1/NameDemo.java
@@ -0,0 +1,16 @@
+package package1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NameDemo {
+	
+	@Test
+	public void test (){
+		String firstName="Harsh";
+		String lastName="Kumar";
+		System.out.println("Hello "+firstName+" "+lastName);
+		Assert.assertTrue(true);
+	}
+
+}
